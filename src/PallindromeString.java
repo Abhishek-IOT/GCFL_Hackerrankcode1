979d66bddface5e01a1095b4471133ef34f52d33
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-01-26 13:07:57 +0530

    create method pallindrome for checking that it is pallidrome or not

--- src/PallindromeString.java
+++ src/PallindromeString.java
@@ -12,4 +12,17 @@ public class PallindromeString {
     public void setString(String string) {
         this.string = string;
     }
+
+    public Boolean Pallindrome(String string, int l) {
+        String reverse = "";
+        for (int i = l; i > 0; i--) {
+            char ch = string.charAt(i);
+            reverse = reverse + ch;
+        }
+        if (reverse.equals(string)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
