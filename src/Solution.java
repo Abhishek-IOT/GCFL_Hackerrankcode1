ccec7501520b66b1de949111e40b78a10f4b3546
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-01-27 15:50:56 +0530

    create an array of size n and check that the sum of elements in array is equal to no of user input

--- src/Solution.java
+++ src/Solution.java
@@ -4,6 +4,22 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int Test = sc.nextInt();
-
+        for (int i = 0; i < Test; i++) {
+            int n = sc.nextInt();
+            int nb = sc.nextInt();
+            int[] array = new int[n];
+            for (int j = 0; j < n; j++) {
+                array[j] = sc.nextInt();
+            }
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum = sum + array[j];
+            }
+            if (sum == nb || sum < nb) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
     }
 }
