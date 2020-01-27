67d43699599a8310f451984994cec179c90acc25
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-01-27 15:49:31 +0530

    import scanner class and create a user defined field that is test

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Test = sc.nextInt();
+
+    }
 }
